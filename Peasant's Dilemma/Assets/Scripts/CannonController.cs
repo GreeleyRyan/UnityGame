9e26c4ae6da98676a28244f9bd36f7fd603c08ff
GreeleyRyan <ryan.greeley16@gmail.com>
2021-03-23 13:41:37 -0230

    added collision and destruction
    cannon hitting peasants now check if they are knocked over and destroys them

--- Peasant's Dilemma/Assets/Scripts/CannonController.cs
+++ Peasant's Dilemma/Assets/Scripts/CannonController.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class CannonController : MonoBehaviour
 {
-    private float horizontalInput;
-    private float verticalInput;
+    public GameObject cannonBall;
+    //public ParticleSystem cannonSmoke;
+
+    private float rotationSpeed = 50.0f;
+    private bool isCannonReady;
+    private Rigidbody cannonRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isCannonReady = true;
+        cannonRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.right, verticalInput * rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isCannonReady)
+        {
+            FireCannon();
+            StartCoroutine(CannonCooldown());
+        }
+    }
+
+    void FireCannon()
+    {
+        Instantiate(cannonBall, new Vector3(0f,3.0f,1.9f), cannonBall.transform.rotation);
+        isCannonReady = false;
+    }
+
+    IEnumerator CannonCooldown()
+    {
+        yield return new WaitForSeconds(3);
+        isCannonReady = true;
     }
 }
---
+++ Peasant's Dilemma/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    private float speed = 50.0f;
+    private Rigidbody cannonBallRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cannonBallRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cannonBallRB.AddForce(Vector3.forward * speed * Time.deltaTime, ForceMode.Impulse);
+    }
+}
