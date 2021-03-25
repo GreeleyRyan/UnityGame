796d405a3526e3f77f9d5e79563070c7bea8b2a9
GreeleyRyan <ryan.greeley16@gmail.com>
2021-03-25 14:35:37 -0230

    fixed cannons
    updated them cannons

--- Peasant's Dilemma/Assets/Scripts/CannonController.cs
+++ Peasant's Dilemma/Assets/Scripts/CannonController.cs
@@ -6,83 +6,142 @@ using TMPro;
 
 public class CannonController : MonoBehaviour
 {
-    public GameObject cannonBall;
+    //UI properties
     public TextMeshProUGUI currentPowerText;
     public TextMeshProUGUI cannonReadyText;
     public TextMeshProUGUI currentAngleText;
-    public Transform muzzle;
-    //public ParticleSystem cannonSmoke;
+    private Camera mainCamera;
 
-    private float minPower = 0f;
-    private float maxPower = 100.0f;
-    private float currentPower;
-    private int currentAngle;
+    //cannon properties
+    public int speed;
+    public float friction;
+    public float lerpSpeed;
+    private float xDegrees;
+    private float yDegrees;
+    private Quaternion fromRotation;
+    private Quaternion toRotation;
     private bool isCannonReady;
+
+    //canonball properties
+    //public ParticleSystem explosion;
+    public GameObject cannonBall;
+    public Transform frontOfBarrel;
+    public float firePower = 50.0f;
     private Rigidbody cannonBallRB;
+
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = Camera.main;
         isCannonReady = true;
-        currentPowerText.text = "Power: " + minPower;
+        currentPowerText.text = "Current Power" + 50;
+        currentAngleText.text = "Current Angle: " + 0;
         cannonReadyText.text = "Cannon is Ready!";
-        currentAngleText.text = "Current Angle: " + currentAngle;
-        cannonBallRB = cannonBall.GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && isCannonReady)
+        UpdateAngleText();
+        CalculatePower();
+        UpdatePowerText();
+        
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
         {
-            CalculateAngle();
-            CalculatePower();
-        } 
-        else if (Input.GetMouseButtonUp(0) && isCannonReady)
+            if (hit.transform.gameObject.tag == "Cannon")
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    CalculateAngle();
+                }
+            }
+        }
+        if(Input.GetKey(KeyCode.Space) && isCannonReady)
         {
-            FireCannon((int) currentPower * 500);
+            FireCannon();
             StartCoroutine(CannonCooldown());
         }
-    }
 
-    void FireCannon(int power)
+    }
+    void FireCannon()
     {
-        Instantiate(cannonBall, muzzle.transform.position + new Vector3(1f,2f,3f), muzzle.transform.rotation);
-        cannonBallRB.AddForce(transform.right * power, ForceMode.Impulse);
+        GameObject cannonBallCopy = Instantiate(cannonBall, frontOfBarrel.position, transform.rotation) as GameObject;
+        cannonBallRB = cannonBallCopy.GetComponent<Rigidbody>();
+        cannonBallRB.AddForce(transform.forward * firePower, ForceMode.Impulse);
+        //Instantiate(explosion, frontOfBarrel.position, frontOfBarrel.rotation);
         isCannonReady = false;
         cannonReadyText.text = "Hang on!";
     }
 
     IEnumerator CannonCooldown()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(2);
+        cannonReadyText.text = "Ready in: 3";
+        yield return new WaitForSeconds(1);
+        cannonReadyText.text = "Ready in: 2";
+        yield return new WaitForSeconds(1);
+        cannonReadyText.text = "Ready in: 1";
+        yield return new WaitForSeconds(1);
         isCannonReady = true;
         cannonReadyText.text = "Cannon is Ready!";
     }
 
     void CalculateAngle()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePosition - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        UpdateAngle((int) angle);
+        xDegrees -= Input.GetAxis("Mouse Y") * speed * friction;
+        yDegrees += Input.GetAxis("Mouse X") * speed * friction;
+
+        //limit rotation angles
+        if(xDegrees > 0)
+        {
+            xDegrees = 0;
+        }
+        else if( xDegrees < -60)
+        {
+            xDegrees = -60;
+        }
+
+        if(yDegrees < -30)
+        {
+            yDegrees = -30;
+        }
+        else if(yDegrees > 30)
+        {
+            yDegrees = 30;
+        }
+
+        fromRotation = transform.rotation;
+        toRotation = Quaternion.Euler(xDegrees, yDegrees, 0);
+        transform.rotation = Quaternion.Lerp(fromRotation, toRotation, Time.deltaTime * lerpSpeed);
     }
 
     void CalculatePower()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        float distance = Vector3.Distance(mousePosition, transform.position);
-        UpdatePower((int) distance);
+        float verticalInput = Input.GetAxis("Vertical");
+        firePower += verticalInput;
+        if(firePower > 100)
+        {
+            firePower = 100;
+        }
+        else if(firePower < 0)
+        {
+            firePower = 0;
+        }
     }
 
-    void UpdatePower(int amount)
+    void UpdatePowerText()
     {
-        currentPower = Mathf.Clamp(amount, minPower, maxPower);
-        currentPowerText.text = "Power: " + currentPower;
+
+        currentPowerText.text = "Power: " + ((int) firePower);
     }
 
-    void UpdateAngle(int angle)
+    void UpdateAngleText()
     {
-        currentAngle = angle;
-        currentAngleText.text = "Current Angle: " + currentAngle;
+        
+        currentAngleText.text = "Current Angle: " + (-(int) xDegrees);
     }
 }
